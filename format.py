5c663179d195833ab4f9d0b1c27a4595c931cfc6
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-19 12:47:52 -0500

    Add files via upload

---
+++ format.py
@@ -0,0 +1,27 @@
+import os, time, pickle
+
+def formatData(loc, sizeTreshold):
+	files = os.listdir(loc)
+	data = {}
+	i = 0
+	for filename in files:
+		i += 1
+		print(i)
+		filename = loc + f'\\{filename}'
+		if os.path.getsize(filename) > sizeTreshold:
+			f = open(filename, 'r').read().strip().split('\n')
+			for fi in f:
+				uid, date, lat, lon = fi.split(',')
+				uid, date, lat, lon = int(uid), time.strftime(date[:-3]), float(lat), float(lon)
+
+				if date not in data:
+					data[date] = [(uid, lat, lon)]
+				else:
+					data[date].append((uid, lat, lon))
+	return data
+
+data = formatData(os.getcwd() + '\\taxi_log_2008_by_id', 10000)
+
+f = open("COVIDdata.pickle", "wb")
+pickle.dump(data, f)
+f.close()
