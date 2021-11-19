b4767eb35e885c0b3b2cb8b07480ef9b11c1ae55
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2021-11-19 14:01:02 -0500

    Update testData.py

--- testData.py
+++ testData.py
@@ -3,3 +3,10 @@ import pickle
 f = open('COVIDdata.pkl', 'rb')
 data = pickle.load(f)
 print(data)
+
+def closeness(long1, lat1, long2, lat2):
+    distance = math.sqrt((long1-long2)**2 + (lat1-lat2)**2)
+    if distance < 0.5:
+        return True
+    else:
+        return False
