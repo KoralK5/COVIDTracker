23c219417325773f3e0eba30856afe16f35f0a41
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-19 14:18:28 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,12 +1,25 @@
-import pickle
+import pickle, math
 
-def closeness(long1, lat1, long2, lat2):
-    distance = math.sqrt((long1-long2)**2 + (lat1-lat2)**2)
-    if distance < 0.5:
-        return True
-    else:
-        return False
+def distance(long1, lat1, long2, lat2):
+	return math.sqrt((long1-long2)**2 + (lat1-lat2)**2)
 
-with open('taxi_track_by_time.pkl', 'rb') as f:
-    data = pickle.load(f)
-    f.close()
+def closeness(long1, lat1, long2, lat2, treshold):
+    return distance(long1, lat1, long2, lat2) < treshold
+
+f = open('COVIDdata.pkl', 'rb')
+data = pickle.load(f)
+
+treshold = 0.0005
+close = 0
+for key in data:
+	values = data[key]
+	size = len(values)
+	for p1 in range(0, size):
+		for p2 in range(p1+1, size):
+			long1 = values[p1][1]
+			lat1  = values[p1][2]
+			long2 = values[p2][1]
+			lat2  = values[p2][2]
+			close += closeness(long1, lat1, long2, lat2, treshold)
+	print(key, close)
+
