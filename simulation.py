d11a47b116a6063b74bc4baa8cc9270ffbd7842c
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-19 14:47:56 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,11 +1,11 @@
 import pickle, math, random, os
 
 def people(loc, sizeTreshold):
-	infections = {}
+	people = {}
 	for filename in os.listdir(loc):
 		if os.path.getsize(loc + f'\\{filename}') > sizeTreshold:
-			infections[int(filename[:filename.index('.')])] = False
-	return infections
+			people[int(filename[:filename.index('.')])] = False
+	return people
 
 def distance(long1, lat1, long2, lat2):
 	return math.sqrt((long1-long2)**2 + (lat1-lat2)**2)
@@ -16,13 +16,12 @@ def closeness(long1, lat1, long2, lat2, treshold):
 f = open('COVIDdata.pkl', 'rb')
 data = pickle.load(f)
 
-infections = people(os.getcwd() + '\\taxi_log_2008_by_id', 10000)
-initial = 10
+people = people(os.getcwd() + '\\taxi_log_2008_by_id', 10000)
+initial = 100
 for i in range(initial):
-	infections[random.choice(list(infections.items()))] = True
+	people[random.choice(list(people.items()))] = True
 
 treshold = 0.000005
-close = 0
 for key in data:
 	values = data[key]
 	size = len(values)
@@ -30,9 +29,9 @@ for key in data:
 		for p2 in range(p1+1, size):
 			long1, lat1 = values[p1][1], values[p1][2]
 			long2, lat2 = values[p2][1], values[p2][2]
-			close = closeness(long1, lat1, long2, lat2, treshold)
+			interact = closeness(long1, lat1, long2, lat2, treshold)
 
-			if close and (infections[values[p1][0]] or infections[values[p2][0]]):
-				infections[values[p1][0]] = True
-				infections[values[p2][0]] = True
+			if interact and (people[values[p1][0]] or people[values[p2][0]]):
+				people[values[p1][0]] = True
+				people[values[p2][0]] = True
 				print(values[p1][0], values[p2][0])
