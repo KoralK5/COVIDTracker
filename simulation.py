d83ff5e6d763b87a7cd40b6be74f890a6e6e1d72
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-19 14:37:55 -0500

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,4 +1,11 @@
-import pickle, math
+import pickle, math, random, os
+
+def people(loc, sizeTreshold):
+	infections = {}
+	for filename in os.listdir(loc):
+		if os.path.getsize(loc + f'\\{filename}') > sizeTreshold:
+			infections[int(filename[:filename.index('.')])] = False
+	return infections
 
 def distance(long1, lat1, long2, lat2):
 	return math.sqrt((long1-long2)**2 + (lat1-lat2)**2)
@@ -9,6 +16,7 @@ def closeness(long1, lat1, long2, lat2, treshold):
 f = open('COVIDdata.pkl', 'rb')
 data = pickle.load(f)
 
+infections = people(os.getcwd() + '\\taxi_log_2008_by_id', 10000)
 treshold = 0.000005
 close = 0
 for key in data:
@@ -16,10 +24,11 @@ for key in data:
 	size = len(values)
 	for p1 in range(0, size):
 		for p2 in range(p1+1, size):
-			long1 = values[p1][1]
-			lat1  = values[p1][2]
-			long2 = values[p2][1]
-			lat2  = values[p2][2]
-			close += closeness(long1, lat1, long2, lat2, treshold)
-	print(key, close)
+			long1, lat1 = values[p1][1], values[p1][2]
+			long2, lat2 = values[p2][1], values[p2][2]
+			close = closeness(long1, lat1, long2, lat2, treshold)
 
+			if close and (infections[p1] or infectionsp[p2]):
+				infections[p1] = True
+				infections[p2] = True
+	print(infections)
