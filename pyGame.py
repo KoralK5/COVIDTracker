a1b0c1c9d246707061aacf1000b502e095b3ff68
Lune3x3 <70852368+Lune3x3@users.noreply.github.com>
2021-11-20 09:40:35 -0500

    Update pyGame.py

--- pyGame.py
+++ pyGame.py
@@ -1,4 +1,9 @@
-import pygame
+import pygame, pickle, os
+
+for filename in os.listdir(os.getcwd()):
+	with open(filename, 'rb') as f:
+		data = pickle.load(f)
+		data = {i:data[i] for i in sorted(data)}
 
 bg = pygame.image.load('BeijingStreetMap.png')
 infected = pygame.image.load('redDot.png')
@@ -7,13 +12,13 @@ healthy = pygame.image.load('whiteDot.png')
 area = 1
 
 def healthCheck(val):
-    if val:
-        return infected
-    else:
-        return healthy
+	if val:
+		return infected
+	else:
+		return healthy
 
 for _ in range(len(data)):
-    blitList = []
-    for i in range(len(list(data)[i])):
-        blitList = blitList.append(infected[_], list(data.values())[1:2], area)
-    pygame.Surface.blits(blitList)
+	blitList = []
+	for i in range(len(list(data)[i])):
+		blitList = blitList.append(infected[_], list(data.values())[1:2], area)
+	pygame.Surface.blits(blitList)
