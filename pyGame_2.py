94348c9c48c3800671623497c9dce3f66ff8ca2f
VMKCreeper <92611907+VMKCreeper@users.noreply.github.com>
2021-11-20 12:11:32 -0500

    Update pyGame_2.py

--- pyGame_2.py
+++ pyGame_2.py
@@ -6,25 +6,29 @@ with open("2008-02-02_13_30.pkl", 'rb') as f:
         
 location = []
 for i in list(data):
-    location.append(data[i][1:])
+    location.append(data[i][1:3])
 
 # top left = 40.193113, 115.796458
 # bottom right = 39.725749, 116.844279
 
-# x = 0.467364
-# y = 1.04781
-# 
+
+x = -2738.7646
+y = 977.266
+#0.44
+
+# long - 40.193113 * x
+# lat - 115.796458 * y
 # 1080 1920
 
 background_image = pygame.image.load("maps_shot.png")
-IMAGE_SMALL = pygame.transform.scale(background_image, (800, 600))
+IMAGE_SMALL = pygame.transform.scale(background_image, (1280, 1024))
 pygame.init()
 
 BGCOLOR = (255, 255, 255)
 RED = (255, 0, 0)
 
 FPS = 30
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1280, 1024))
 pygame.display.update()
 
 clock = pygame.time.Clock()
@@ -44,7 +48,7 @@ while running:
     screen.blit(IMAGE_SMALL, [0, 0])
     
     for i in range(len(location)):
-        pygame.draw.circle(screen, RED, [location[i][1], location[i][0]], 1)
+        pygame.draw.circle(screen, RED, [(location[i][1] - 40.193113) * x, (location[i][0] - 115.796458) * y], 1)
         
     pygame.display.flip()
 
