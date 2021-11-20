524508dc1450d35662b4238bab492c1a7c3e6849
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-20 12:42:03 -0500

    Update pyGame_2.py

--- pyGame_2.py
+++ pyGame_2.py
@@ -1,55 +1,43 @@
-import pygame, pickle, random
-
-with open("2008-02-02_13_30.pkl", 'rb') as f:
-    data = pickle.load(f)
-    data = {i:data[i] for i in sorted(data)}
-        
-location = []
-for i in list(data):
-    location.append(data[i][1:3])
-
-# top left = 40.193113, 115.796458
-# bottom right = 39.725749, 116.844279
-
+import pygame, pickle, os
 
 x = -2738.7646
 y = 977.266
-#0.44
 
-# long - 40.193113 * x
-# lat - 115.796458 * y
-# 1080 1920
-
-background_image = pygame.image.load("maps_shot.png")
+background_image = pygame.image.load(f'{os.getcwd()}\\Images\\maps_shot.png')
 IMAGE_SMALL = pygame.transform.scale(background_image, (1280, 1024))
 pygame.init()
 
 BGCOLOR = (255, 255, 255)
 RED = (255, 0, 0)
-
+BLUE = (0, 0, 255)
 FPS = 30
+
 screen = pygame.display.set_mode((1280, 1024))
 pygame.display.update()
-
 clock = pygame.time.Clock()
 
+def grab(filename):
+	return pickle.load(open(f'{os.getcwd()}\\spreadData\\{filename}', 'rb'))
+
+files = os.listdir(os.getcwd() + '\\spreadData')
+
+f = 0
 running = True
-
 while running:
+	clock.tick(FPS)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
 
-    clock.tick(FPS)
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            pygame.quit()
-
-    screen.fill(BGCOLOR)
-    screen.blit(IMAGE_SMALL, [0, 0])
-    
-    for i in range(len(location)):
-        pygame.draw.circle(screen, RED, [(location[i][1] - 40.193113) * x, (location[i][0] - 115.796458) * y], 1)
-        
-    pygame.display.flip()
+	screen.fill(BGCOLOR)
+	screen.blit(IMAGE_SMALL, [0, 0])
+	
+	data = grab(files[f])
+	for i in data.keys():
+		color = RED if data[i][0] else BLUE
+		pygame.draw.circle(screen, color, [(data[i][1] - 40.193113) * x, (data[2][0] - 115.796458) * y], 1)
+	
+	pygame.display.flip()
+	f += 1
 
 pygame.quit()
