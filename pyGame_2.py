502d31219693114cfc2216156a237dad23007546
Koral <62809012+KoralK5@users.noreply.github.com>
2021-11-21 10:22:29 -0500

    Update pyGame_2.py

--- pyGame_2.py
+++ pyGame_2.py
@@ -24,6 +24,7 @@ files = os.listdir(os.getcwd() + '\\spreadData')
 f = 0
 running = True
 while running:
+	clock.tick(FPS)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
@@ -31,12 +32,18 @@ while running:
 	screen.fill(BGCOLOR)
 	screen.blit(IMAGE_SMALL, [0, 0])
 	
+	infected, uninfected = 0, 0
 	data = grab(files[f])
 	for i in data.keys():
 		color = RED if data[i][0] else BLUE
+		infected += data[i][0]
+		uninfected += not(data[i][0])
 		pygame.draw.circle(screen, color, [(data[i][2] - 40.193113) * x, (data[i][1] - 115.796458) * y], 2)
 	
+	t = files[f].split(' ')
+	t = f'{t[0]} {t[1]}:{t[2][:2]} ----- {uninfected} uninfected ----- {infected} infected'
 	pygame.display.flip()
+	pygame.display.set_caption(t)
 	f += 1
 
 pygame.quit()
